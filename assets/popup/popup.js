f0cae08e4eab425a8b9b8cb3b8fe0ba2e99e0c71
cliobox <cliobox@outlook.com>
2017-06-03 06:19:38 -0500

    cliobox
    primero

---
+++ assets/popup/popup.js
@@ -0,0 +1,23 @@
+  /*function inicio()
+  {
+      document.getElementById('pop1').style.display='block';
+  }*/
+
+
+  function cerrarPopup(pid)
+     {
+        document.getElementById(pid).style.display = 'none';      
+     }
+   
+
+  function lanzadera()
+    {
+          // document.getElementById('myModal').style.display='block';
+      }
+      
+  window.onload = lanzadera;
+
+  function gotox()
+    {
+          window.open("http://www.cliobox.com", "_blank");
+      }
