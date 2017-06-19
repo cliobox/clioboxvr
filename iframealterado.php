1f432b066e30deac064863ae93fa48b5f92d6456
cliobox <cliobox@outlook.com>
2017-06-19 15:07:44 -0500

    iframe

---
+++ iframealterado.php
@@ -0,0 +1,6 @@
+<?php
+$url = curl_init("http://www.google.es");
+<style>.link-back img {display: none !important;}</style><base href='https://kuula.co/share/7lG4B?fs=0&amp;zoom=1&amp;logo=0'>";
+$salida = curl_exec ($url);
+curl_close($url);
+?>
